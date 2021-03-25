7b05ae57434be27be3b333f13b039fb67b439a4c
Raphael Grimm <raphael.grimm@kit.edu>
2021-03-25 16:36:03 +0100

    Separate main and particle simulation

--- src/src_particle_simulation/HW1.cpp
+++ src/src_particle_simulation/HW1.cpp
@@ -6,11 +6,6 @@
 //
 ///////////////////////////////////////////////////////////////////////////////
 
-#define BRUTE1
-
-#define SCENARIO_DAM      0
-#define SCENARIO_FAUCET   1
-#define SCENARIO_CUBE     2
 
 #include <cstdlib>
 #include <sys/time.h>
